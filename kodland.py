f3d9a2012c6d24509982a96f9c0e352d95fac264
Soda228654 <sasha.bulanov.00@inbox.ru>
2025-02-15 16:37:45 +0300

    kodland_bot.py
    bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot_bot_Bot

--- kodland.py
+++ kodland.py
@@ -1,14 +1,49 @@
+import telebot
+from main import gen_pass
+from smalik import gen_small
+token = ""    
+    # Замени 'TOKEN' на токен твоего бота
+    # Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot(token)
 
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "РОФЛ": "шутка",
-            "ЩИЩ": "одобрение или восторг",
-            "КРИПОВЫЙ": "страшный, пугающий",
-            "АГРИТЬСЯ": "злиться"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("такое слово даже мы не знаем")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)    
+
+@bot.message_handler(commands=['kakdela'])
+def send_dela(message):
+    bot.reply_to(message, "[хорошо, а твои?]")
+
+@bot.message_handler(commands=['pass'])
+def send_pass(message):
+    bot.reply_to(message, f"Boт ваш пароль: {gen_pass(10)}")
+
+@bot.message_handler(commands=['smaliki'])
+def send_small(message):
+    bot.reply_to(message, f"Boт ваш : {gen_small(10)}")
+
+@bot.message_handler(commands=['stop'])
+def send_stop(message):
+    bot.send_message(message.chat.id, "бот выключен")
+    bot.stop_bot()
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+print("bot start")
+bot.polling()
